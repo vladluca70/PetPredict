c5be1ea9dee3cbb847e2b0347abbc8bee9c74e9c
vladluca70 <vladluca70@gmail.com>
2025-08-23 18:18:08 +0300

    login and signup on expressjs

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,71 @@ const userSchema = new mongoose.Schema({
 const userModel=mongoose.model('User', userSchema)
 
 
+router.post('/login', async(req, res) => {
+    try {
+        const username=req.body.username
+        const password=req.body.password
+
+         const usernameExists=await userModel.findOne({username:username})
+        if(usernameExists){
+            const isPasswordValid= await bcrypt.compare(password, usernameExists.password)
+            if(isPasswordValid){
+                console.log("password valid")
+                res.status(200).json({message:"log in succesful"})
+            }
+            else{
+                console.log("password invalid")
+                res.status(401).json({message:"password invalid"})
+            }
+        }
+        else{
+            console.log("user not found in database")
+            res.status(401).json({message:"user not found in database"})
+        }        
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+app.post('/signup', async(req, res)=>{
+    const username=req.body.username
+    const password=req.body.password
+
+    if(!username){
+        return res.status(400).json({message:"username is required"})
+    }
+    if(!password){
+        return res.status(400).json({message:"password is required"})
+    }
+
+    try {
+        const usernameExists= await userModel.findOne({username:username})
+        if(usernameExists){
+            console.log(`${username} already exists in database`);
+            return res.status(409).json({ message: "username already exists" });
+        }
+        else{
+            const salt = await bcrypt.genSalt(10); 
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            const newUser = new userModel({
+                username: username,
+                password: hashedPassword
+            });
+
+            await newUser.save();
+
+            console.log(`${username} has been registered successfully`);
+            return res.status(201).json({ message: "user registered successfully" });
+        }
+
+    } catch (error) {
+        console.error("signup server error", error);
+        res.status(500).json({ message: "internal server error" });
+    }
+});
+
 app.listen(
     port, ()=>{
         console.log(`it is listening on port ${port}`)
--- frontend/pet-predict/src/LoginPage.jsx
+++ frontend/pet-predict/src/LoginPage.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function LoginPage() {
   const [username, setUsername]=useState('')
   const [password, setPassword]=useState('')
+  const [errorMessage, setErrorMessage]=useState('')
 
   function handleUsernameChange(e){
     setUsername(e.target.value)
@@ -12,13 +13,39 @@ function LoginPage() {
     setPassword(e.target.value)
   }
 
+  async function handleLoginType() {
+    await sendData('login')
+  }
+
+  async function sendData(requestType) {
+    const url=`http://localhost:5002/${requestType}`
+    try {
+      const response=await fetch(url, {
+        method:'POST',
+        headers:{'Content-Type':"application/json"},
+        body:JSON.stringify({username:username, password:password})
+      });
+      const responseData=await response.json()
+        if(response.ok){
+            setErrorMessage('')
+            //succesfulLoginOrRegister(username)
+        }
+        else{
+            setErrorMessage(responseData.message)
+        }   
+    } catch (error) {
+      console.error('errors', error)
+    }
+  }
+
   return (
     <div>
       <h2>Login Page</h2>
       <form>
         <input required type="text" placeholder="Username" onChange={(e)=>handleUsernameChange(e)}/>
         <input required type="password" placeholder="Password" onChange={(e)=>handlePasswordChange(e)}/>
-        <button type="submit">Login</button>
+        <button type="submit" onClick={handleLoginType}>Login</button>
+        {errorMessage && <p>{errorMessage}</p>}
       </form>
     </div>
   );
